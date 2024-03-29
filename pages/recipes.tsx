c94bf028e7daf694caf5e7434206b0c443fb0a11
Braulio_Bolano <brauliobolanostrujillo@gmail.com>
2024-03-29 06:11:19 -0400

    Added home page

---
+++ pages/recipes.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import Card from '../components/Card/Card'
+import SearchBar from '../components/SearchBar/SearchBar';
+import { GetServerSideProps } from 'next';
+import { DarkThemeToggle } from 'flowbite-react';
+
+
+interface Recipe {
+  id: string;
+  name: string;
+  ingredients: string[];
+  cuisine: string;
+  instructions: string;
+  dietaryPreferences: string[];
+}
+interface HomeProps {
+  initialRecipes: Recipe[];
+}
+
+
+export default function Home({ initialRecipes } : HomeProps) {
+  const [search, setSearch] = useState('');
+  const [filteredRecipes, setFilteredRecipes] = useState(initialRecipes);
+
+  const runSearchFunction = () => {
+    const results = initialRecipes.filter(recipe =>
+      recipe.name.toLowerCase().includes(search.toLowerCase()) ||
+      recipe.ingredients.some(ingredient => ingredient.toLowerCase().includes(search.toLowerCase())) ||
+      recipe.cuisine.toLowerCase().includes(search.toLowerCase()) || 
+      recipe.dietaryPreferences.some(dietaryPreference => dietaryPreference.toLowerCase().includes(search.toLowerCase()))
+    );
+    setFilteredRecipes(results);
+    console.log('runSearchFunction for value of search: ', search);
+  }
+
+  return (
+    <main>
+      <DarkThemeToggle />
+      <h1>Recipe Discovery</h1>
+      <SearchBar search={search} setSearch={setSearch} runSearchFunction={runSearchFunction}/>
+      <div className="flex flex-wrap justify-center">
+        {filteredRecipes.map((recipe) => (
+          <Card key={recipe.id} recipe={recipe} />
+        ))}
+      </div>
+    </main>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/recipes`);
+  const initialRecipes = await res.json();
+
+  if (!initialRecipes) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { initialRecipes }, // will be passed to the page component as props
+  }
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,9 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
+  darkMode: 'class', 
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +17,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin"),
+    
+  ],
 };
 export default config;
