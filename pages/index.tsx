9a5424c09c56078170e7d9475602cd63d9cd8b09
Braulio_Bolano <brauliobolanostrujillo@gmail.com>
2024-03-29 06:26:01 -0400

    Updated home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,16 @@
 import Link from 'next/link';
 import NavigationBar from '../components/NavigationBar/NavigationBar';
 import Hero from '../components/Hero/Hero';
+import Footer from '../components/Footer/Footer';
+import ContentSection from '../components/ContentSection/ContentSection';
 
 export default function Home() {
   return (
     <div>
       <NavigationBar />
       <Hero />
-      <Link href="/recipes">
-        Go to recipes
-      </Link>
+      <ContentSection />
+      <Footer />
     </div>
   );
 }
